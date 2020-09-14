2a34548639e6a20ca690bc5d4e4cfb5f23bfe8d6
zetsme <zetsme182@ukr.net>
2020-09-14 18:05:55 +0300

    add dynamic GET route

--- server.js
+++ server.js
@@ -10,5 +10,13 @@ app.get('/users', (req, res) => {
     users: mockUserData,
   });
 });
+app.get('/users/:id', (req, res) => {
+  console.log(req.params.id);
+  res.json({
+    success: true,
+    message: 'got one user',
+    user: req.params.id,
+  });
+});
 //
 app.listen(8000, () => console.log('server is running'));
