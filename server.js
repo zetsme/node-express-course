f72bef6a7ef2bf98ab6810f5a9760ec81fbcc354
zetsme <zetsme182@ukr.net>
2020-09-14 17:50:34 +0300

    add first GET route

--- server.js
+++ server.js
@@ -2,4 +2,13 @@
 const express = require('express');
 const app = express();
 //
+const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }];
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'successfuly got users. Nice!',
+    users: mockUserData,
+  });
+});
+//
 app.listen(8000, () => console.log('server is running'));
